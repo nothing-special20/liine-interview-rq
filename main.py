14fa04d552fb78b6b3b13d5233dba6e6115dc10c
nothing-special20 <rquin@billmoretech.com>
2024-11-27 07:48:37 -0500

    load restaurant data on startup and persist in state to prevent re-reading on each request, add datetime_str format validation, rename variables

--- main.py
+++ main.py
@@ -1,21 +1,38 @@
-from fastapi import FastAPI, status, Query
+from fastapi import FastAPI, status, Query, Request, HTTPException
 from fastapi.responses import JSONResponse
 import pandas as pd
+from datetime import datetime
 
 from liine_tech_take_home_test import search_open_restaurants, main_etl, file
 
 app = FastAPI()
 
+@app.on_event("startup")
+async def startup_event():
+    """Load and process the data when the application starts"""
+    restaurant_schedules_raw = pd.read_csv(file)
+    app.state.restaurant_schedules = main_etl(restaurant_schedules_raw)
+
 @app.get("/open-restaurants")
-def search(datetime_str: str = Query(default=None, example="2024-11-26 19:30")):
-    raw_data = pd.read_csv(file)
+def search(request: Request, datetime_str: str = Query(default=None, example="2024-11-26 19:30")):
+    if not hasattr(request.app.state, "restaurant_schedules"):
+        return JSONResponse(
+            content={"error": "Server experienced issue loading restaurant schedule."},
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR
+        )
 
-    main_data = main_etl(raw_data)
+    try:
+        datetime.strptime(datetime_str, "%Y-%m-%d %H:%M")
+    except ValueError:
+        raise HTTPException(
+            status_code=400,
+            detail="Invalid datetime format. Use YYYY-MM-DD HH:MM"
+        )
 
-    x = search_open_restaurants(main_data, datetime_str)
+    open_restaurants = search_open_restaurants(request.app.state.restaurant_schedules, datetime_str)
 
     return JSONResponse(
-        content=x,
+        content=open_restaurants,
         media_type="application/json",
         status_code=status.HTTP_200_OK
     )
